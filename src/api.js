22f838d5e34a34736fee0997127d7d44b956ec8a
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-04 09:04:40 -0700

    2nd stage complete

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/calvin-anthony';
+
 
 const getItems = function () {
   return Promise.resolve('A successful response!');
 };
+
 
 export default {
   getItems
